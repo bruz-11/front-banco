7dad2b8a35e6f48460276430f34433edcffdc45f
Scott <scott@skt-t.corylvke.cs@gmail.com>
2026-09-20 01:11:32 -0300

    quedo una mayuscula

--- src/pages/agregarcontacto.jsx
+++ src/pages/agregarcontacto.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import api from '../api'; // <-- IMPORTANTE: Usamos tu instancia configurada, no axios
-import './agregarContacto.css';
+import './agregarcontacto.css';
 
 const AgregarContacto = () => {
     const { userId } = useParams();
